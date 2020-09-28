be27992a46519d8e7d1279e3dcb0dad2f14ffda5
Sergey Zinchenko <s.zinchenko@e-mba.ru>
2020-09-28 16:02:08 +0300

    Improve models

--- src/models/Courses/Course.ts
+++ src/models/Courses/Course.ts
@@ -2,10 +2,11 @@ import { Model } from "../Model"
 import { Course as CourseModel } from "../../interfaces/models/Courses/Course"
 
 export class Course extends Model implements CourseModel {
-    public title: string
-    public alias: string
-    public note: string
-    public description: string
+    public title: string;
+    public alias: string;
+    public note: string;
+    public description: string;
+    public type: string;
 
     constructor(attributes: Map<string, any>) {
         super();
@@ -18,6 +19,7 @@ export class Course extends Model implements CourseModel {
         this.alias = attributes.get('alias');
         this.note = attributes.get('note');
         this.description = attributes.get('description');
+        this.type = attributes.get('type');
 
         return this;
     }
--- src/models/Courses/Task.ts
+++ src/models/Courses/Task.ts
@@ -2,10 +2,10 @@ import { Model } from "../Model"
 import { Task as TaskModel } from "../../interfaces/models/Courses/Task"
 
 export class Task extends Model implements TaskModel {
-    public title: string
-    public alias: string
-    public note: string
-    public description: string
+    public title: string;
+    public alias: string;
+    public note: string;
+    public description: string;
 
     constructor(attributes: Map<string, any>) {
         super();
